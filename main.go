7e3e5b8e6c23419dd99c16894e39812175838a31
Shantanu Raj <shantanuraj@ovi.com>
2014-03-23 03:48:43 +0530

    Ensure input closes even on error returns, add usage/error messages.

--- main.go
+++ main.go
@@ -7,15 +7,20 @@ import (
 )
 
 func main() {
+	if len(os.Args) < 2 {
+		fmt.Printf("Usage: sss <file-name>\n")
+		return
+	}
+
 	input, err := os.Open(os.Args[1])
+	defer input.Close()
 	if err != nil {
-		panic(err)
+		fmt.Printf("Error! Path: %s does not exist.\n", os.Args[1])
+		return
 	}
 
 	// yyDebug = 1
 	lexer := NewLexer(bufio.NewReader(input))
 	yyParse(lexer)
 	fmt.Printf("%v\n", lexer.output)
-
-	input.Close()
 }
